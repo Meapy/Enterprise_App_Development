ca279be18c1e38f58510ff9b63b3598f23c7540a
Meapy <dkrasovskijj@gmail.com>
2022-05-01 15:34:09 +0100

    removed stuff from html

--- Lab5/app.js
+++ Lab5/app.js
@@ -1,11 +1,17 @@
 const express = require('express');
-var router = express.Router();
+const bodyParser = require("body-parser");
+const redis = require('redis');
+const session = require('express-session');
 var methodOverride = require('method-override')
+var router = express.Router();
+
 const app = express();
 const port = 5000;
 const fs = require('fs');
-const bodyParser = require("body-parser");
-const { Router } = require('express');
+
+let RedisStore = require('connect-redis')(session);
+let redisClient = redis.createClient();
+
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.engine('html', require('ejs').renderFile);
@@ -15,14 +21,26 @@ app.use(express.static(__dirname + '/vendor'));
 
 var cookieParser = require('cookie-parser');
 app.use(cookieParser());
+app.use(
+    session({
+        store: new RedisStore({ client: redisClient }),
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: false,
+        cookie: { maxAge: 60000 }
+    })
+);
+
 
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on port ${port}`);
 });
+
 
 // load index.html
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 }
 );
+
 
